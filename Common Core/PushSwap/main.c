08c985f2968e50e33b37c158b30b637975ee3273
Yannis Mosca--Bulain <yamosca-@3F2.42angouleme.fr>
2025-11-23 20:49:07 +0100

    Adding Push Swap Parsing Structure

--- Common Core/PushSwap/main.c
+++ Common Core/PushSwap/main.c
@@ -6,7 +6,7 @@
 /*   By: yamosca- <yamosca-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 20:45:18 by yamosca-          #+#    #+#             */
-/*   Updated: 2025/11/21 21:15:53 by yamosca-         ###   ########.fr       */
+/*   Updated: 2025/11/23 20:48:21 by yamosca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,28 @@
 
 int main(int argc, char **argv)
 {
-    int i;
-    char *stack_a;
-    int n;
-    int j;
-
-    j = 0;
-    n = 0;
-    i = 0;
-    if (argc < 2)
-        return (0);
-    while (n < argc - 1)
+    list stack_a;
+    
+    stack_a->value = argv[i][n]
+    // int et ' '
+    if (ft_is_in(stack_a) == 0)
+        error
+    else
     {
-        if (argv[n][i] == '\0')
-            argv[n][i] = ' ';
-        stack_a[j] = argv[n][i];
-        n++;
-        i++;
+        char **tmp = NULL;
+        int n = 0;
+        long *stack;
+        tmp = split(stack_a->value, ' ')
+        while (tmp[n])
+            stack[n] = ft_atol(tmp[n]);
+            n++;
+        n = 0;
+        while (stack[n])
+            stack_a->value = stack[n];
+            stack_a = stack_a->next;
+            n++;
+            
+        // 
     }
+    
 }
